8e0cf4748beda9ce60cfea149accc8ace0e3c41d
dmswns0147@naver.com <dmswns0147@naver.com>
2023-07-04 14:30:42 +0900

    Initial Commit

---
+++ GrowUp/GrowUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowUpApp.swift
+//  GrowUp
+//
+//  Created by Nicode . on 2023/07/04.
+//
+
+import SwiftUI
+
+@main
+struct GrowUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
